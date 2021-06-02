8595836a24ecb5c48136a599e748ff236f209404
lenowac <lenowac@pm.me>
2021-06-02 11:09:29 +0200

    path prefix and metadata

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -4,12 +4,12 @@ const PATH_PREFIX = process.env.PATH_PREFIX
 
 module.exports = {
   // pathPrefix: PATH_PREFIX || `/foam-template-gatsby-kb`, // a. If you are using github pages, this should be the name of your repo
-  pathPrefix: PATH_PREFIX || `/`, // b. If you are using Netlify/Vercel, your can keep it this way
+  pathPrefix: PATH_PREFIX || `/wiki`, // b. If you are using Netlify/Vercel, your can keep it this way
   siteMetadata: {
     // some SEO configs using by gatsby-theme-kb
-    title: `Foam`, // Replace it with your site's title
-    author: `Your Name`, // Replace it with your name
-    description: `My personal knowledge base`, // Replace it with your site's description
+    title: `Notes`, // Replace it with your site's title
+    author: `lenowac`, // Replace it with your name
+    description: `personal knowledge base`, // Replace it with your site's description
   },
   plugins: [
     {
